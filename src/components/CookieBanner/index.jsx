1c2553b986e52eda3f2a38e1cfbd039922eb305a
Sébastien <seb.juillet987@hotmail.fr>
2024-11-27 15:36:14 +0100

    change cookie component

--- src/components/CookieBanner/index.jsx
+++ src/components/CookieBanner/index.jsx
@@ -1,44 +1,121 @@
-import { useState, useEffect } from "react";
-import {Link} from "react-router-dom";
-import "./index.scss"; 
+// import { useState, useEffect } from "react";
+// import {Link} from "react-router-dom";
+// import "./index.scss";
 
+// const CookieBanner = () => {
+//   const [showBanner, setShowBanner] = useState(false);
+
+//   useEffect(() => {
+//     const cookiesConsent = localStorage.getItem("cookiesConsent");
+//     if (!cookiesConsent) {
+//       setShowBanner(true);
+//     }
+//   }, []);
+
+//   const handleAccept = () => {
+//     localStorage.setItem("cookiesConsent", "true");
+//     setShowBanner(false);
+//   };
+
+//   const handleReject = () => {
+//     setShowBanner(false);
+//   };
+
+//   if (!showBanner) return null;
+
+//   return (
+//     <div className="cookie-banner">
+//       <p>
+//         Nous utilisons des cookies pour améliorer votre expérience. En poursuivant votre navigation, vous acceptez notre{" "}
+//         <Link to="/politique-de-confidentialite">politique de confidentialité</Link>.
+//       </p>
+//       <div className="cookie-actions">
+//         <button onClick={handleAccept} className="cookie-accept">
+//           Accepter
+//         </button>
+//         <button onClick={handleReject} className="cookie-reject">
+//           Refuser
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CookieBanner;
+
+import CookieConsent from "react-cookie-consent";
+// import { Cookies } from "js-cookie";
+import { Link } from "react-router-dom";
+
+import "./index.scss";
 
 const CookieBanner = () => {
-  const [showBanner, setShowBanner] = useState(false);
-
-  useEffect(() => {
-    const cookiesConsent = localStorage.getItem("cookiesConsent");
-    if (!cookiesConsent) {
-      setShowBanner(true);
-    }
-  }, []);
-
   const handleAccept = () => {
-    localStorage.setItem("cookiesConsent", "true");
-    setShowBanner(false);
+    console.log("Cookies acceptés");
+    const script = document.createElement("script");
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-MR1G9EJM3X";
+    script.async = true;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        window.dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+      gtag("config", "G-MR1G9EJM3X");
+    };
   };
 
   const handleReject = () => {
-    setShowBanner(false);
+    console.log("Cookies refusés");
+    // Cookies.remove("user-consent");
   };
 
-  if (!showBanner) return null;
-
   return (
-    <div className="cookie-banner">
-      <p>
-        Nous utilisons des cookies pour améliorer votre expérience. En poursuivant votre navigation, vous acceptez notre{" "}
-        <Link to="/politique-de-confidentialite">politique de confidentialité</Link>.
-      </p>
-      <div className="cookie-actions">
-        <button onClick={handleAccept} className="cookie-accept">
-          Accepter
-        </button>
-        <button onClick={handleReject} className="cookie-reject">
-          Refuser
-        </button>
-      </div>
-    </div>
+    <CookieConsent
+      location="bottom"
+      buttonText="Accepter"
+      declineButtonText="Refuser"
+      cookieName="user-consent"
+      expires={30}
+      onAccept={handleAccept}
+      enableDeclineButton={true}
+      onDecline={handleReject}
+      // style={{
+      //   background: "#2B373B",
+      //   color: "#fff",
+      //   fontSize: "16px",
+      //   height: "100px",
+      //   width:"1440px",
+      //   display: "flex",
+      //   flexDirection: "column",
+      //   // alignItems: "center",
+      //   // justifyContent: "center",
+      // }}
+      buttonStyle={{
+        backgroundColor: "#4e8c2f",
+        color: "#fff",
+        fontSize: "16px",
+        padding: "10px 20px",
+        borderRadius: "5px",
+      }}
+      declineButtonStyle={{
+        backgroundColor: "#f44336",
+        color: "#fff",
+        fontSize: "16px",
+        padding: "10px 20px",
+        borderRadius: "5px",
+      }}
+    >
+      Nous utilisons des cookies pour améliorer votre expérience. En poursuivant
+      votre navigation, vous acceptez notre
+      <Link to="/politique-de-confidentialite" className="link-cookie">
+        {" "}
+        politique de confidentialité
+      </Link>
+      .
+    </CookieConsent>
   );
 };
 
